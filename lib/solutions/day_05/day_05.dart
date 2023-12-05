9a647c8f77adf6864f4f50e1fe961adfab1e6ef6
Degover <chaors2000@gmail.com>
2023-12-05 19:51:36 -0300

    Added files to solve day 05

---
+++ lib/solutions/day_05/day_05.dart
@@ -0,0 +1,14 @@
+import 'package:advent_of_code/shared/input.dart';
+import 'package:advent_of_code/solutions/solution.dart';
+
+class DayFive implements Solution {
+  @override
+  Future<String> firstPart(Input input) async {
+    return 'unresolved';
+  }
+
+  @override
+  Future<String> secondPart(Input input) async {
+    return 'unresolved';
+  }
+}
