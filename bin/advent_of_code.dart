7f13f7cd478dba730e280e2a9749cf8f3f7c9124
Degover <chaors2000@gmail.com>
2025-08-16 11:42:34 -0300

    Small changes (hopefully)

--- bin/advent_of_code.dart
+++ bin/advent_of_code.dart
@@ -1,5 +1,10 @@
-import 'package:advent_of_code/advent_of_code.dart' as advent_of_code;
+import 'package:advent_of_code/shared/file_input.dart';
+import 'package:advent_of_code/solutions/day_05/day_05.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${advent_of_code.calculate()}!');
+void main(List<String> arguments) async {
+  final fileInput = FileInput('./inputs/05.txt');
+  final solution = DayFive();
+
+  final result = await solution.secondPart(fileInput);
+  print(result);
 }
